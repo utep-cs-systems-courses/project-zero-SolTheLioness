18cf059620d0cb4bcbbd87a0a1fe926da7ecfd56
SolTheLioness <jalara10@miners.utep.edu>
2021-09-12 12:44:47 -0600

    Edit draw_chars.c with a new function that uses the provided 5x7-font.c bitmap to print characters.Added reference to new function in draw.h, and added controller functionality in uimain.c. Finally, added the font object (?) to the Makefile.

--- uimain.c
+++ uimain.c
@@ -7,7 +7,7 @@ int main()
 
   while (1) { // Infinite while loop
 
-    fputs("Select which shape you want to print (Arrow = a, Triangle = t, Square = s, Chars = c) or 'q' to quit\n> ", stdout);
+    fputs("Select which shape you want to print (Arrow = a, Triangle = t, Square = s, Chars = c, Custom Font Chars = C, Compare chars = x) or 'q' to quit\n> ", stdout);
     fflush(stdout);		/* stdout only flushes automatically on \n */
     int c;
     while ((c = getchar()) == '\n'); /* ignore newlines */
@@ -30,8 +30,22 @@ int main()
       break;
     case 'c':
       puts("You selected chars:");
-      for (char c = 'A'; c < 'D'; c++)
+      for (char c = 'A'; c < 'G'; c++)
 	print_char_11x16(c);
+      break;
+    case 'C':
+      puts("You selected custom font chars:");
+      for (char c = 'A'; c <= 'Z'; c++)
+	print_char_5x7(c);
+      break;
+    case 'x':
+      puts("You selected to compare fonts:");
+      for (char c = 'A'; c <= 'Z'; c++) {
+	if (c % 2)
+	  print_char_11x16(c);
+	else
+	  print_char_5x7(c);
+      }
       break;
     case 'q':
       puts("Bye!");
